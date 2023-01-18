5b90fc887e53036562235eea6f72909a640a2c8a
Rishat_Imangulov <gr40in@yandex.ru>
2023-01-18 23:02:40 +0500

    end Task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -8,5 +8,20 @@
 
 Console.Clear();
 
-Console.Write("Введите трёхзначное число (100-999): ");
-int x1 = int.Parse(Console.ReadLine());
+bool errorInput = true;
+int inputNumber = 1;
+while (errorInput)
+{
+    Console.Write("Введите трёхзначное число (100-999): ");
+    inputNumber = int.Parse(Console.ReadLine());
+    if (inputNumber < 100 || 999 < inputNumber)
+    {
+        errorInput = true;
+        Console.Write("Ошибка ввода. ");
+        continue;
+    }
+    else
+        errorInput = false;
+}
+//                                   abc число. Искомое - b  = >   abc / 10 = ab    ab % 10 = b 
+Console.Write($" Вы ввели число {inputNumber}. Искомая 2-a цифра: {(inputNumber / 10) % 10}");
