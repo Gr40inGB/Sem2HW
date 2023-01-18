ab7c918bfa9073286be5959435eed4f1374b9efe
Rishat_Imangulov <gr40in@yandex.ru>
2023-01-19 02:49:30 +0500

    end Task 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -8,6 +8,24 @@
 Console.Clear();
 
 Console.Write("Введите число: ");
-inputNumber = int.Parse(Console.ReadLine());
+String inputNumberString = Console.ReadLine();
 
-Console.Write("Введенное число: {inputNumber}. третья цифра {inputNumber/100}");
+int inputNumberInt = 0;
+
+try
+{   // преобразование как учились на семинаре. Но тогда, есть проблемы с большими числами
+    inputNumberInt = Convert.ToInt32(inputNumberString);
+}
+catch (System.Exception)
+{   // здесь вырезаем нужный размер строки, чтобы дальнейший код сработал. 
+    // Да и напрашивается такое решение. Правда я не стал обрабатывать случай, если введут число с запятой. 
+    inputNumberInt = Convert.ToInt32(inputNumberString.Substring(inputNumberString.Length - 3));
+}
+
+Console.Write($"Введенное число: {inputNumberString}. ");
+
+if (-99 < inputNumberInt && inputNumberInt < 100)  // только в этом случае не будет 3-й цифры
+    Console.Write($" Третьей цифры нет");
+else
+    // a - числа до. B -искомое. сс - две цифры после  aa..aaBcc/100 = aa..aaB.  aa..aaB % 10 = B   
+    Console.Write($" третья цифра {(inputNumberInt / 100) % 10}");
